b08fc4ae436ebead6d58fe0d1acff419b805b70a
Malcolm Toulouse <malcolmtoulouse@Malcolms-Laptop-8.local>
2026-07-30 16:51:28 -0400

    Update piece coordinates when moving

---
+++ engine/move.py
@@ -0,0 +1,25 @@
+"""
+move.py
+
+Represents a chess move.
+"""
+
+
+class Move:
+
+
+    def __init__(
+        self,
+        start,
+        end,
+    ):
+
+        self.start = start
+        self.end = end
+
+
+    def __repr__(self):
+
+        return (
+            f"{self.start} -> {self.end}"
+        )
